48cfb771532ce339d4e790e10b1c3018995b3c6f
onuohak <143371922+onuohak@users.noreply.github.com>
2024-01-02 17:41:30 -0600

    Create config.js

---
+++ Leaflet-Part-1/config.js
@@ -0,0 +1,3 @@
+// Store your API key here for testing purposes
+// If deploying to production, use environment variables to secure the key
+const api_key = "pk.eyJ1IjoicmFiZWxsYW4iLCJhIjoiY2xxY3lsbGkzMDNlMjJrbW9rY3FwajhoOCJ9.L2T0Am71fGxGZAQmoaewUw";
